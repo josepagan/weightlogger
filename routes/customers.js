9d96b06b4b18b9ae6c94a53127bcca52556db757
Jose Pagan <jm.pagan@gmail.com>
2019-06-09 12:58:53 +0100

    customer api now receives weight embedded

--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Customer, validateCustomer } = require('../models/customer');
+const { Weight } = require('../models/weight');
 
 const router = express.Router();
 
@@ -7,5 +8,26 @@ router.get('/', async (req, res) => {
   const customers = await Customer.find().lean();
   res.send(customers);
 });
+
+router.post('/', async (req, res) => {
+  // add validation joi here
+  const newCustomer = new Customer({
+    name: req.body.name,
+    email: req.body.email,
+    language: req.body.language,
+    displayUnit: req.body.displayUnit,
+  });
+
+  await newCustomer.save();
+  res.send(`New customer ${newCustomer.name} saved!`);
+});
+
+router.put('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+  customer.weights.push(new Weight({ weight: req.body.weight }));
+  customer.save();
+  res.send(`this is ${customer.name}`);
+});
+
 
 module.exports = router;
